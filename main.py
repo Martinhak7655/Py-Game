beb5233903ef811b2fa1fd4087a03ebd2e627a2c
Martinhak7655 <martinhakobyan954@gmail.com>
2024-09-14 14:04:59 +0400

    Updates elif operating system

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def gameu(randint):
     user = int(input("Greq Dzer  Tivy"))
     if user == randint:
         print("Apres Duq haxteciq")
-    elif user == 5:
+    elif user >= 5:
         print("Minchev 1 ic 4 tvery")
     else:
         print("Cavoq Duq partveciq")
